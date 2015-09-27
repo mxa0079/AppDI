8b6833b62697a1c55057b7aaca29ce14d799dec1
mau <mauricio.avi@outlook.com>
2015-09-26 23:24:48 -0700

    Renamed test class to correctly represent purpose

---
+++ AppDi/AppDiTests/AppDriverFabricTests/AppDriverFactory_Dynamic_PageObjectRegistration.cs
@@ -0,0 +1,37 @@
+﻿namespace AppDiTests.AppDriverFabricTests
+{
+    using AppDi;
+    using System.Diagnostics;
+    using System.Dynamic;
+    using Xunit;
+
+    public class AppDriverFactory_Dynamic_PageObjectRegistration
+    {
+        private AppDriverFactory SUT;
+
+        public AppDriverFactory_Dynamic_PageObjectRegistration()
+        {
+            SUT = AppDriver.Factory();
+        }
+
+        [Fact]
+        [Trait("SUT", "AppDriverFactory")]
+        public void Works_Reistering_A_Single_Page_At_A_time()
+        {
+            //This is the URL configured on appdi.config.json and hence it should be the URL value of our Concrete page object
+            //See test "Creates_AppDriver_taking_defaults_from_JSON_config()" for details
+            const string JSON_CONFIG_URL = @"http://www.google.com/";
+
+            //Register a Page Object with our app driver
+            dynamic driver = SUT.Register<ConcretePageObject>().Create();
+
+            driver.Concrete.IncreaseServiceCallCounterByOne();
+
+            //Ensure we have access to the dynamic page object we just registered
+            //And that it was created as expected
+            Assert.Equal(JSON_CONFIG_URL, driver.Concrete.Url.ToString());
+
+        }
+
+    }
+}
